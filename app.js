c0ff0decafa0fe69e7a7d2f1db89e9a15f81a42b
khadarbashajilan <khadarbashajilan001@gmail.com>
2025-03-02 05:32:52 +0530

    Update app.js
    * 4

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function levelUp() {
         highscore = level;
     }
     h2.innerText = `Level ${level}`;
-    let randIdx = Math.floor(Math.random() * 3);
+    let randIdx = Math.floor(Math.random() * 4);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
     gameSeq.push(randColor);
